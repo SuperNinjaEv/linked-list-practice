da2e9c19f653459b59d272405098fd747867604c
SuperNinjaEv <evan0210@gmail.com>
2023-06-26 21:58:10 -0400

    Created classes for Node and LinkedList

--- solution.js
+++ solution.js
@@ -1,5 +1,21 @@
 const { nums, words } = require("./data/data.js");
 
+// NODE
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+};
+
+// LINKED LIST
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+}
+
+
 module.exports = {
   Node,
   LinkedList,
