d9199130a62dbbea24a87001a694c5eab183092c
SuperNinjaEv <evan0210@gmail.com>
2023-06-27 18:13:11 -0400

    Completed insert method according to testing requirements, wanted insert at front

--- solution.js
+++ solution.js
@@ -5,15 +5,22 @@ class Node {
   constructor(data) {
     this.data = data;
     this.next = null;
-  }
+  };
 };
 
 // LINKED LIST
 class LinkedList {
   constructor(head = null) {
     this.head = head;
+  };
+
+  insert(data, position){
+    let newNode = new Node(data);
+
+    newNode.next = this.head;
+    this.head = newNode;
   }
-}
+};
 
 
 module.exports = {
