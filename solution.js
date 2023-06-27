78d4e02f2a81009db8c058e03e5a4199392a1df8
SuperNinjaEv <evan0210@gmail.com>
2023-06-27 18:23:29 -0400

    Completed getLast method

--- solution.js
+++ solution.js
@@ -53,6 +53,15 @@ class LinkedList {
 
   getFirst() {
     return this.head;
+  };
+
+  getLast() {
+    let currentNode = this.head;
+
+    while( currentNode.next != null) {
+      currentNode = currentNode.next;
+    };
+    return currentNode;
   }
 };
 
